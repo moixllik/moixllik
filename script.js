3adaedff00ec7526f135822c06fbcf6439bad3cd
Irving Montalvo <moixllik@gmail.com>
2024-07-29 23:42:12 +0000

    fixed date

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ fetch('https://us-east4.gcp.data.mongodb-api.com/app/application-0-dojzngj/endpo
     method: 'POST', body: q || '*'
 }).then(r => r.json()).then(list => {
     $result.innerHTML = list.map(
-        e => `<p><a href="${e.h}" target="_blank"><b>${e.t}</b></a> | <i>${e.l.toUpperCase()}</i> | <small>${e.d}</small></p>`
+        e => `<p><a href="${ e.h }" target="_blank"><b>${ e.t }</b></a> | <i>${ e.l.toUpperCase() }</i> | <small>${ e.d.split('T')[0] }</small></p>`
     ).join('')
 })
